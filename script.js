a0e74d4a7b3b5523c3afbaba57e622a3a82dacc2
nataliewxngg <wong.lt.natalie@gmail.com>
2025-01-09 11:50:25 -0500

    Completed the backend logic for basic operations

--- script.js
+++ script.js
@@ -7,8 +7,10 @@ const EQUALBUTTONBG = "#57cc99";
 
 let operation = "";
 let result = "";
-let newOp = "";
-let secondOp = false;
+let newOp = true;
+
+let nums = [];
+let operators = [];
 
 const buttons = document.querySelectorAll("button");
 const numButtons = document.querySelectorAll(".nums");
@@ -19,90 +21,70 @@ const equalButton = document.querySelector(".equal");
 const operationText = document.querySelector("#operation-text");
 const resultText = document.querySelector("#result-text")
 
-const operations = {
-    mod: " % ",
-    divide: " / ", 
-    minus: " - ",
-    add: " + ",
-    multiply: " * ",
-    decimal: "."
-}
-
-function operate(x, y, op) {
-    if (op == "+") {
-        return parseFloat(x) + parseFloat(y);
-    } else if (op == "-") {
-        return parseFloat(x) - parseFloat(y);
-    } else if (op == "*") {
-        return parseFloat(x) * parseFloat(y);
-    } else {
-        return parseFloat(x) / parseFloat(y);
-    }
-}
-
-// Event Listeners (HOVER)
+// EVENT LISTENERS
 buttons.forEach((button) => {
-    button.addEventListener("mouseover", () => {
-        button.style.transform = "scale(1.1)";
-    })
+    button.addEventListener("mouseover", () => button.style.transform = "scale(1.1)");
     button.addEventListener("mouseout", () => {
         button.style.transform = "scale(1)";
         button.style.background = DEFAULTBUTTONBG;
-    })
-})
+    });
+});
 
-numButtons.forEach((numButton) => {
-    numButton.onmouseover = () => numButton.style.background = NUMBUTTONBG;
-})
-opButtons.forEach((opButton) => {
-    opButton.onmouseover = () => opButton.style.background = OPBUTTONBG;
-})
-clearButtons.forEach((clearButton) => {
-    clearButton.onmouseover = () => clearButton.style.background = CLEARBUTTONBG;
-})
+numButtons.forEach(numButton => numButton.onmouseover = () => numButton.style.background = NUMBUTTONBG);
+opButtons.forEach(opButton => opButton.onmouseover = () => opButton.style.background = OPBUTTONBG);
+clearButtons.forEach(clearButton => clearButton.onmouseover = () => clearButton.style.background = CLEARBUTTONBG);
 equalButton.onmouseover = () => equalButton.style.background = EQUALBUTTONBG;
 
-// Event Listeners (CLICK)
 buttons.forEach((button) => {
     button.addEventListener("click", function (e) {
         if (e.target.id == "all-clear-button") {
             operation = "0";
-            secondOp = false;
             result = "";
         } else if (e.target.id == "clear-button") {
-            if (operation.charAt(operation.length - 1) == ' ') {
-                operation = operation.substring(0, operation.length-2);
-                // console.log("true");
-            } else
-                operation = operation.substring(0, operation.length-1);
-        } else if (e.target.id == "mod-button"||e.target.id == "divide-button"||e.target.id == "minus-button" || e.target.id == "add-button" || e.target.id == "multiply-button" ||e.target.id == "decimal-button") {
-            // console.log(operations[e.target.id.substring(0, e.target.id.indexOf("-"))])
-            if (secondOp) { 
-                result = operate(result, operation.substring(operation.lastIndexOf(' ') + 1, operation.length), newOp);
-                operation = "";
-                newOp = operations[e.target.id.substring(0, e.target.id.indexOf("-"))].trim();
-                
-                console.log(result);
-                console.log(newOp);
-            } else {
-                result = operation;
-                operation += operations[e.target.id.substring(0, e.target.id.indexOf("-"))];
-                newOp = operations[e.target.id.substring(0, e.target.id.indexOf("-"))].trim();
-            }
-            secondOp = true;
+
+
+            if (operation.charAt(operation.length - 1) == ' ')
+                operation = operation.substring(0, operation.length - 2);
+            else
+                operation = operation.substring(0, operation.length - 1);
+
+        } else if (e.target.id == "mod-button" || e.target.id == "divide-button" || e.target.id == "minus-button" || e.target.id == "add-button" || e.target.id == "multiply-button") {
+            operators.push(e.target.id.substring(0, e.target.id.indexOf('-')));
+            newOp = true;
+            console.log(operators);
         } else if (e.target.id == "equal-button") {
-            result = operate(operation, result.substring(0,result.indexOf(' ')), result.substring(result.indexOf(' ')+1,result.lastIndexOf(' ')));
-            
-            operation = "0";
-            operating = "";
-            secondOp = false;
+            if (!(nums.length - 1 == operators.length)) 
+                result = 'MATH ERROR';
+
+            result = 0;
+            for (let operator of operators) {
+                console.log(Number(nums[0]));
+                console.log(Number(nums[1]));
+
+                if (operator == "add") 
+                    result = Number(nums[0]) + Number(nums[1]);
+                else if (operator == "minus") 
+                    result = Number(nums[0]) - Number(nums[1]);
+                else if (operator == "multiply")
+                    result = Number(nums[0]) * Number(nums[1]);
+                else  
+                    result = Number(nums[0]) / Number(nums[1]);
+                
+                nums.splice(0, 1);
+                nums[0] = result;
+            }
         } else {
-            operation += e.target.id.substring(0,e.target.id.indexOf('-'));
+            newOp
+                ? nums.push(e.target.id.substring(0, e.target.id.indexOf('-')))
+                : nums[nums.length-1] = nums[nums.length - 1] += e.target.id.substring(0, e.target.id.indexOf('-')); 
+            newOp = false;
+            console.log(nums);
         }
         
-        if (operation.trim() == "") operation = " ";
+        if (operation.trim() == "") operation = "0"; 
         operationText.textContent = operation.toString();
-        if (secondOp)
-            resultText.textContent = result.toString();
-    })
-})
+
+        console.log(result);
+        resultText.textContent = result; 
+    });
+});

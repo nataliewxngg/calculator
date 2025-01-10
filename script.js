65fd40ff3fdc17a3e15a141fb4afc852964dec2c
nataliewxngg <wong.lt.natalie@gmail.com>
2025-01-10 14:05:27 -0500

    Removed leading 0s

--- script.js
+++ script.js
@@ -7,10 +7,9 @@ const EQUALBUTTONBG = "#57cc99";
 
 let operation = "";
 let result = "";
-let newOp = true;
-
 let nums = [];
 let operators = [];
+let newOp = true;
 let modifyOperation = true;
 
 const buttons = document.querySelectorAll("button");
@@ -39,13 +38,11 @@ equalButton.onmouseover = () => equalButton.style.background = EQUALBUTTONBG;
 buttons.forEach((button) => {
     button.addEventListener("click", function (e) {
         if (e.target.id == "all-clear-button") {
-            modifyOperation = true;
-            operation = "";
-            result = "";
+            // reset variable values
+            operation = result = "";
             nums = [];
             operators = [];
-            newOp = true;
-
+            newOp = modifyOperation = true;
         } else if (e.target.id == "clear-button") {
             modifyOperation = true;
             if (newOp && nums.length > 0) {
@@ -55,6 +52,7 @@ buttons.forEach((button) => {
             else {
                 if (nums.length > 0) {
                     nums[nums.length - 1] = Number(nums[nums.length - 1].toString().substring(0, nums[nums.length - 1].toString().length - 1))
+                    // remove the number from nums completely if it becomes 0
                     if (nums[nums.length - 1] == 0) {
                         nums.pop();
                         newOp = true;
@@ -64,28 +62,31 @@ buttons.forEach((button) => {
         } else if (e.target.id == "%-button" || e.target.id == "/-button" || e.target.id == "--button" || e.target.id == "+-button" || e.target.id == "*-button") {
             if (nums.length > 0) {
                 modifyOperation = true;
-                if (newOp) operators.pop();
+                if (newOp) operators.pop(); // remove previously entered operator to replace with new operator if necessary
                 operators.push(e.target.id.substring(0, 1));
                 newOp = true;
                 
-                // calculate first 2 numbers if available
+                // first calculate the result from the first 2 numbers if available
                 if (nums.length > 1) {
                     calculate();
                     operators.splice(0, 1);
                 }
             } else {
+                // allow for negative numbers
                 if (e.target.id == "--button") {
                     nums.push(e.target.id.substring(0, 1));
                     newOp = false;
                 }
             }
         } else if (e.target.id == "equal-button") {
+            // utilize the value of result as the 2nd # if there is a trailing operator in the operation
             if (nums.length == operators.length && modifyOperation) {
                 if (result == "") result = 0;
                 nums.push(Number(result));
                 operation += ` ${result} `
             }
 
+            // prevents calculation from running if user clicks on equal button multiple times
             if (modifyOperation) {
                 calculate();
                 newOp = true;
@@ -93,14 +94,15 @@ buttons.forEach((button) => {
             modifyOperation = false;
         } else {
             modifyOperation = true;
-            newOp
-                ? nums.push(e.target.id.substring(0, e.target.id.indexOf('-')))
-                : nums[nums.length - 1] += e.target.id.substring(0, e.target.id.indexOf('-')); 
-            newOp = false;
-        }
 
-        console.log(`operators: [${operators}]`);
-        console.log(`numbers: [${nums}]`);
+            if (newOp && e.target.id.substring(0, e.target.id.indexOf('-')) != "0") {
+                nums.push(e.target.id.substring(0, e.target.id.indexOf('-')));
+                newOp = false;
+            } else if (!newOp) {
+                nums[nums.length - 1] += e.target.id.substring(0, e.target.id.indexOf('-')); 
+                newOp = false;
+            }
+        }
 
         // displays the operation and result on the webpage
         if (modifyOperation) {
